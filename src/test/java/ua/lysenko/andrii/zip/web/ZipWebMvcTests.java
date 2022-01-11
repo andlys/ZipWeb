65a88455a80a1d5ca635c083baa76328be5f8049
andlys <senkoily@gmail.com>
2022-01-11 11:29:20 +0200

    fix unit tests

--- src/test/java/ua/lysenko/andrii/zip/web/ZipWebMvcTests.java
+++ src/test/java/ua/lysenko/andrii/zip/web/ZipWebMvcTests.java
@@ -4,6 +4,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -13,7 +14,9 @@ public class ZipWebMvcTests {
 
     @Autowired
     private MockMvc mockMvc;
-/*
+    @MockBean
+    private StorageService storageService;
+
     @Test
     public void contextLoads() {
     }
